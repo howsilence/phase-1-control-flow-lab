51a277a6e10b92efb9cb7bd21faff8a0fbe69093
howsilence <statton0104@gmail.com>
2022-01-10 17:37:46 -0500

    commit tree a8d19c19055d684f9bc3a92de138149e043293c7

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400){
+  return 'This one is on me!'
+  } else if (feet <= 2499) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (feet >= 2500) {
+    return 'No can do.'
+  }
+};
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return (city === "NYC" ? "Ok, sounds good." : "No go.");
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(generous){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  
+  switch (generous) {
+    case 'generous' : "Thank you so much.";
+    break;
+    case 'not as generous' : "Thank you."
+    break;
+    default: "Bye.";
+  }
 }
